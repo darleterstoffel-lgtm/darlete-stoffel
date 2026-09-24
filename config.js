f1bd41fad1395ca597a52de2b05014969f590394
darleterstoffel-lgtm <darleterstoffel@gmail.com>
2026-09-24 12:33:01 -0300

    Adicionar links do mini site

---
+++ config.js
@@ -0,0 +1,5 @@
+window.DARLETE_CONFIG = {
+  whatsappUrl: "https://wa.me/5527988196136?text=Ol%C3%A1%2C%20Darlete%21%20Tudo%20bem%3F%20Gostaria%20de%20saber%20mais%20sobre%20as%20sess%C3%B5es%20de%20psicoterapia%20com%20voc%C3%AA.",
+  instagramUrl: "https://www.instagram.com/darletestoffel/",
+  ebookUrl: "https://drive.google.com/file/d/1I1ODKz_HbQNlXm3xEu3fIaqjpUdZSzcO/view?usp=drivesdk"
+};
